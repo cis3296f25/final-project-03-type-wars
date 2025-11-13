656e31d60c7b27c1acde7642fc41f4a4cfde0e49
Alexis Graham <tul31142@temple.edu>
2025-11-13 14:52:20 -0500

    Wins command completed

--- type-wars-bot.py
+++ type-wars-bot.py
@@ -31,9 +31,17 @@ bot.remove_command("help")
 
 GUILD_ID = discord.Object(id=1420829218882719848)
 
+
 @bot.event
 async def on_ready():
+    global users
     print(f"Logged on as {bot.user.name}!")
+    try:
+        with open("leaderboard.json", 'r') as f:
+            users = json.load(f)
+    except FileNotFoundError:
+        users = {}
+        print("No win data file found. Starting with empty win data.")
 
 @bot.event
 async def on_message(message):
@@ -64,10 +72,6 @@ async def help(ctx):
 
 
     await ctx.send(embed = embed)
-
-
-#wins = 0
-
 
 @bot.command(name="typewars", description="Challenge someone to a typing duel!")
 async def typewars(ctx):
@@ -161,40 +165,37 @@ async def typewars(ctx):
         if msg.content.strip().lower() == word.lower():
             speed = round(end_time - start_time, 2)
 
-            # add_win()
             await ctx.send(f"Nice job {ctx.author.mention}! You typed it correctly in **{speed} seconds!**")
+            await add_win(ctx)
+
         else:
             await ctx.send(f"The correct word was **{word}**.")
 
 @bot.command(name = "wins", description = "Check how many wins you have")
 async def wins(ctx):
 
-    users = await leaderboard_data()
-    id = str(ctx.message.author.id)
+    id = str(ctx.author.id)
 
-    if id not in await leaderboard_data():
-        await ctx.send("❓You haven't played yet! Play some games to see your score! ")
+    if id not in users:
+        await ctx.send("❓ You haven't played yet! Play some games to see your score! ")
     else:
-        await ctx.send("🎖️ You have {} wins in TypeWars!".format(users[id]))
+        await ctx.send("🎖️  You have {} win(s) in TypeWars!".format(users[id]))
 
-
-async def leaderboard_data():
-    with open("leaderboard.js", "r") as f:
-        users = await json.load(f)
-
-    return users
 
 async def add_win(ctx):
 
-    users = await leaderboard_data()
-    id = str(ctx.message.author.id)
+    id = str(ctx.author.id)
 
-    if id not in users():
+    if id not in users:
         users[id] = 0
 
     users[id] += 1
+    save_data()
 
-    with open('leaderboard.json', 'w+') as f:
-         json.dump(users, f)
+async def save_data():
+    global users
+    with open("leaderboard.json", 'w') as f:
+        json.dump(users, f, indent=2)
+
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
